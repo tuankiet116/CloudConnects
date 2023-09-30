ab0ff2f492d9e9ba0775981b6b0c125c5defb4af
tuankiet116 <kietdat1612000@gmail.com>
2023-09-30 20:43:55 +0700

    feat: style layout navbar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,12 @@
 <template>
     <header class="w-full">
-        <div class="flex">
-            <img class="w-4" src="../assets/images/storage-linkers-logo.png"/>
-            <div class="float-right">
-                <nuxt-link to="/">Home</nuxt-link>
-                <nuxt-link to="/">About Us</nuxt-link>
-                <nuxt-link to="login">Login</nuxt-link>
-            </div>
+        <div class="">
+            <img class=" w-20" src="../assets/images/storage-linkers-logo.png"/>
+            <nav class="flex justify-center gap-x-4">
+                <nuxt-link class="border font-bold p-2" to="/">Home</nuxt-link>
+                <nuxt-link class="border font-bold p-2" to="/">About Us</nuxt-link>
+                <nuxt-link class="border font-bold p-2" to="login">Login</nuxt-link>
+            </nav>
         </div>
     </header>
     <slot/>
