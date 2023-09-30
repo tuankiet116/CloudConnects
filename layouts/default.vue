9a57808f29e014b3de2f4a67c6017e58bc5366e7
tuankiet116 <kietdat1612000@gmail.com>
2023-09-30 20:32:13 +0700

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+    <header class="w-full">
+        <div class="flex">
+            <img class="w-4" src="../assets/images/storage-linkers-logo.png"/>
+            <div class="float-right">
+                <nuxt-link to="/">Home</nuxt-link>
+                <nuxt-link to="/">About Us</nuxt-link>
+                <nuxt-link to="login">Login</nuxt-link>
+            </div>
+        </div>
+    </header>
+    <slot/>
+</template>
+<script lang="ts">
+ 
+</script>
+<style scoped>
+header {
+    max-height: 200px;
+}
+</style>
---
+++ layouts/userLayout.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        This is user layout
+    </div>
+    <slot/>
+</template>
+<script setup>
+</script>
+<style scoped>
+</style>
